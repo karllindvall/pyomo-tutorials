48cc729ad25d1f3e3ef5740526602d9a59952eda
karllindvall <karljlindvall@gmail.com>
2025-09-05 13:58:04 +0200

    completed non-linear exercise 1.1
    implemented a range of initial x-vals for the rosenbrock fxn

--- exercises/Nonlinear/exercises-1/rosenbrock.py
+++ exercises/Nonlinear/exercises-1/rosenbrock.py
@@ -14,13 +14,20 @@
 import pyomo.environ as pyo
 
 model = pyo.ConcreteModel()
-model.x = pyo.Var(initialize=1.5)
-model.y = pyo.Var(initialize=1.5)
+model.x = pyo.Var()
+model.y = pyo.Var()
 
 def rosenbrock(model):
     return (1.0-model.x)**2 \
         + 100.0*(model.y - model.x**2)**2
 model.obj = pyo.Objective(rule=rosenbrock, sense=pyo.minimize)
 
-pyo.SolverFactory('ipopt').solve(model, tee=True)
-model.pprint()
+y_val = 1.5
+for i in range(2,6):
+    model.x = i
+    model.y = y_val
+    pyo.SolverFactory('ipopt').solve(model, tee=True)
+
+    print("{0:6.2f}  {1:6.2f}  {2:6.2f}  {3:6.2f}".format(i, \
+            y_val, pyo.value(model.x), pyo.value(model.y)))
+    
